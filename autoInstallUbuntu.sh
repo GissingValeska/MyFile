d2bed57d9f7a9f744a5fdfe41b9b2cc0466af0ac
GissingValeska <3011371536@qq.com>
2022-03-13 10:48:52 +0800

    auto install ubuntu

---
+++ autoInstallUbuntu.sh
@@ -0,0 +1,4 @@
+#sudo apt update
+#sudo apt upgrade
+sudo apt install neofetch neovim figlet cowsay ranger git wget curl screenkey simplescreenrecorder variety markdown 
+sudo apt install vlc rofi apt-file flameshot
