0be8ed36b4a0e410911d33ccb20a1a870c09ec3e
GissValeska <3011371536@qq.com>
2022-03-18 14:10:51 +0800

    整理

--- autoInstallUbuntu.sh
+++
@@ -1,4 +0,0 @@
-#sudo apt update
-#sudo apt upgrade
-sudo apt install neofetch neovim figlet cowsay ranger git wget curl screenkey simplescreenrecorder variety markdown 
-sudo apt install vlc rofi apt-file flameshot
